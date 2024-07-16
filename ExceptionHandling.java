df1c9158dc7bf629d9f0a5ccd12f120775e9c08c
Shaili <shailidwivedi05@gmail.com>
2024-07-17 02:41:38 +0530

    implemented nested catch block and multicatch block for exception handling

--- ExceptionHandling.java
+++ ExceptionHandling.java
@@ -13,5 +13,31 @@ public class ExceptionHandling{
         }
         System.out.println(13);
 
+        // Nested try catch block
+        int [] arr = {2,4,5,9};
+        try {
+            System.out.println(4/0);
+            // flow will not go here 
+            System.out.println(arr[10]);
+        }catch(ArithmeticException ae){
+            System.out.println("Arithmetic exception: "+ ae);
+        }catch(NullPointerException npe){
+            System.out.println("this exception will not occur");
+        }catch(ArrayIndexOutOfBoundsException aio){
+            System.out.println("ArrayIndexOutofBound : "+ aio);
+        }
+        catch (Exception e) {
+            System.out.println("your exception: " + e);
+        }
+
+        // multicatch block java 7
+        try {
+            System.out.println(arr[10]);
+        }catch(ArithmeticException | ArrayIndexOutOfBoundsException | NullPointerException e ){
+            System.out.println("Arithmetic exception: "+ e);
+        }
+
+
+
     }
 }
